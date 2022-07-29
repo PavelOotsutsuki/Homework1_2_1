6b5606e80d11600d75d905f9e5eb64a07cb80444
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-29 18:22:16 +0300

    Добавьте файлы проекта.

---
+++ Homework1_2_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int count;
+
+            Console.Write("Введите сообщение, которое должно выводиться: ");
+            message = Console.ReadLine();
+            Console.Write("Введите, сколько раз Вы хотите чтобы оно выводилось: ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
